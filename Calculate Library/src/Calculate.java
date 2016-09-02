af40357f7a2b04cf8497154d7604e7080113e6e4
Whyisthissohard <kevinwong1846@gmail.com>
2016-09-02 09:48:32 -0700

    Math Library Part Uno

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -6,12 +6,20 @@
  * @author Kevin
  *
  */
+
 public class Calculate {
-	
+
 	public static int square(int number){
-		int answer;
-		answer = number * number;
-		return answer;
+	 return (number * number);
 	}
-	
+	public static int cube(int number){
+		return(number*number*number);
+	}
+	public static double average(double number,double number2){
+		return((number+number2)/2); 
+	}
+	public static double average(double number,double number2,double number3){
+		return((number+number2+number3)/2);
+	}
 }
+
