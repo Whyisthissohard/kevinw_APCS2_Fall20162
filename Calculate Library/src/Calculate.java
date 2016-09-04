391380d5fcab6628532352d701cfa8e5d87b17bd
Whyisthissohard <kevinwong1846@gmail.com>
2016-09-04 10:26:12 -0700

    Put the changes Made

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -13,5 +13,15 @@ public class Calculate {
 		answer = number * number;
 		return answer;
 	}
+	public static int cube(int number){
+		return (number * number * number);
+	}
+	public static double average(double number ,double number1){
+		return ((number * number1)/2);		
+	}
+	public static double average(double number , double number2, double number3){
+		return (number * number2 * number3);
+	}
+
 	
 }
